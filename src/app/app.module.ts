2fc19f2b0eb6c6522b83d5739972ad9e6a1efdc7
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-11-02 09:26:52 +0200

    1

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProductComponent } from './core/product/product.component';
+import { ProductDetailsComponent } from './core/product/product-details/product-details.component';
+import { HomeComponent } from './core/home/home.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { bootstrapCart4 } from '@ng-icons/bootstrap-icons';
+import { CartComponent } from './core/cart/cart.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OrderComponent } from './core/order/order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    HomeComponent,
+    CartComponent,
+    OrderComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgIconsModule.withIcons({ bootstrapCart4 }),
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
